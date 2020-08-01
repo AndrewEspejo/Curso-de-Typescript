a9d82569639e3735ae2cf57608769ddbb160d44d
ANdres espejo <jeaespejogo@unal.edu.co>
2020-08-01 00:23:45 -0500

    types

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,21 @@
 "use strict";
 console.log("Web development is my new passion!");
+//Tipos en TypeScript
+//Type Number
+//Explícito
+let phone;
+phone = 15;
+phone = 2544;
+//Inferido
+let phoneNumber = 2547;
+phoneNumber = 124;
+// phoneNumber=true //Error por tipo
+//Hexadecimal
+let hex = 0xf00d;
+//Binario
+let binary = 0b1010;
+//Octal
+let octal = 0o744;
+//Type Boolean
+let isPro;
+isPro = true;
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,25 @@
 console.log("Web development is my new passion!");
+
+//Tipos en TypeScript
+
+//Type Number
+//Explícito
+let phone: number;
+phone = 15;
+phone = 2544;
+
+//Inferido
+let phoneNumber = 2547;
+phoneNumber = 124;
+// phoneNumber=true //Error por tipo
+
+//Hexadecimal
+let hex: number = 0xf00d;
+//Binario
+let binary: number = 0b1010;
+//Octal
+let octal: number = 0o744;
+
+//Type Boolean
+let isPro: boolean;
+isPro = true;
